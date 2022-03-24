ae7f4aa323cf0dc13b406869f1461e9b44dff9da
kidolu <54096690+devolusegun@users.noreply.github.com>
2022-03-24 13:45:44 +0100

    Update CodeineToken.sol

--- CodeineToken.sol
+++ CodeineToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract CodeIne is ERC20 {
+contract CodeineToken is ERC20 {
     constructor() ERC20("Codeine Junk Yard", "CJY") {
         _mint(msg.sender, 1000 * 10 ** 3);
     }
